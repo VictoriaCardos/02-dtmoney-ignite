99752621732b75171eaa5b008d7553e74f085d75
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-20 22:23:51 -0300

    feat: add modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { Dashboard } from './components/Dashboard';
 import {Header} from './components/Header'
+import Modal from 'react-modal'
 import {GlobalStyle} from './styles/global';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root'); //isso é só pra acessibilidade
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal}/>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle/>
     </>
